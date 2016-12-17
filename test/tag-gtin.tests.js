c3a2c429538a83c669a553413e3463b11d2a0e51
vegfjogs <vegard.fjogstad@finn.no>
2016-12-17 16:06:11 +0100

    Generic test and node_modules to gitignore.

---
+++ test/tag-gtin.tests.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+
+
+describe('Tag-gtin', function() {
+  describe('mod10control()', function() {
+    it('should return correct number', () => {
+      assert.equal(1, 1);
+    });
+  });
+});
