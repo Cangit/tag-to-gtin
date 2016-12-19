acb65173ab634c22871419744d8d18185cf4018f
Fredric Endrerud <fredricendrerud@gmail.com>
2016-12-19 14:48:47 +0100

    :panda_face: semistandard js compliance

--- tag-gtin.js
+++ tag-gtin.js
@@ -1,42 +1,42 @@
-const epc = require('node-epc')
+const epc = require('node-epc');
 
 const RFID_GTIN = class {
 
   mod10control (num) {
-    let calc
-    let checksum = 0
-    const r = [2, 1]
+    let calc;
+    let checksum = 0;
+    const r = [2, 1];
     if (typeof num === 'number') {
-      num = num.toString()
+      num = num.toString();
     }
 
     for (var i = num.length - 1; i--;) {
-      calc = num.charAt(i) * r[i % r.length]
-      calc = ((calc / 10) | 0) + (calc % 10)
-      checksum += calc
+      calc = num.charAt(i) * r[i % r.length];
+      calc = ((calc / 10) | 0) + (calc % 10);
+      checksum += calc;
     }
 
-    const controlNumber = (10 - (checksum % 10)) % 10
+    const controlNumber = (10 - (checksum % 10)) % 10;
 
-    return controlNumber
+    return controlNumber;
   }
 
   tagToGtin (rfidHex, callback) {
-    const _this = this
+    const _this = this;
     epc.getParser('sgtin').then(function (sgtin) {
       sgtin.parse(rfidHex).then(function (parsed) {
-        const id = parsed.parts.ItemReference.substring(1, 6)
-        const tag = parsed.parts.CompanyPrefix + id
-        const controlNumber = _this.mod10control(tag)
+        const id = parsed.parts.ItemReference.substring(1, 6);
+        const tag = parsed.parts.CompanyPrefix + id;
+        const controlNumber = _this.mod10control(tag);
 
-        callback(tag + controlNumber)
+        callback(tag + controlNumber);
       }).fail(function (err) {
-        console.error(err)
-      })
-    })
+        console.error(err);
+      });
+    });
   }
 
-}
+};
 
-const rfidGtin = new RFID_GTIN()
-module.exports = rfidGtin
+const rfidGtin = new RFID_GTIN();
+module.exports = rfidGtin;
--- test/tag-gtin.tests.js
+++ test/tag-gtin.tests.js
@@ -4,32 +4,31 @@ const rfidGtin = require('../tag-gtin.js');
 const assert = require('assert');
 
 describe('Tag-gtin', () => {
-    describe('mod10control()', () => {
-        describe('should return correct control number with', () => {
-            it('string literal', () => {
-                const expected = 9;
-                const actual = rfidGtin.mod10control('6747553');
-                assert.equal(expected, actual);
-            });
+  describe('mod10control()', () => {
+    describe('should return correct control number with', () => {
+      it('string literal', () => {
+        const expected = 9;
+        const actual = rfidGtin.mod10control('6747553');
+        assert.equal(expected, actual);
+      });
 
-            it('numeric value', () => {
-                const expected = 9;
-                const actual = rfidGtin.mod10control(6747553);
-                assert.equal(expected, actual);
-            });
-        });
-
+      it('numeric value', () => {
+        const expected = 9;
+        const actual = rfidGtin.mod10control(6747553);
+        assert.equal(expected, actual);
+      });
     });
+  });
 
-    describe('tagToGtin', () => {
-        describe('should create gtin tag when feeded with rfid tag', () => {
-            it('tag string', (done) => {
-                rfidGtin.tagToGtin('3035E8BF60548AED2276206E', function (gtin) {
-                  const expected = '8007640865718'
-                  if (expected === gtin) done()
-                  else done(new Error('expected: ' + expected + ', got: ' + gtin))
-                })
-            })
-        })
-    })
+  describe('tagToGtin', () => {
+    describe('should create gtin tag when feeded with rfid tag', () => {
+      it('tag string', (done) => {
+        rfidGtin.tagToGtin('3035E8BF60548AED2276206E', function (gtin) {
+          const expected = '8007640865718';
+          if (expected === gtin) done();
+          else done(new Error('expected: ' + expected + ', got: ' + gtin));
+        });
+      });
+    });
+  });
 });
